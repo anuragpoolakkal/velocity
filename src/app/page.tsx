0dc758e4a56572d4bd1e096f22150261a8d002be
anuragrajanp <anuragrajanp@gmail.com>
2023-10-01 17:34:45 +0530

    content
    implement functionality including heading, sub heading, button and timer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 "use client";
-
+import Image from "next/image";
+import { useEffect, useState, useRef } from "react";
 import { assets } from "@/utils/asset-utils";
 import { type Framework, frameworks } from "@/utils/framework-utils";
 import { cn } from "@/utils/tailwind-utils";
+import { FrameworkRotation } from "@/components/FrameworkRotation";
 import { Poppins } from "next/font/google";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { CountdownTimer } from "@/components/CountdownTimer";
 
 const poppins = Poppins({
 	weight: "700",
 	subsets: ["latin"],
 });
-
 export default function Home() {
 	const [currentFramework, setCurrentFramework] = useState<Framework>(frameworks[0]);
 	const [showBackground, setShowBackground] = useState(false);
+	const buttonRef = useRef<HTMLButtonElement>(null);
 
 	useEffect(() => {
 		let currentIndex = 0;
@@ -46,7 +47,6 @@ export default function Home() {
 					"bg-neutral-300": currentFramework === "desktop",
 				})}
 			/>
-
 			{/* Grid */}
 			<div
 				style={{
@@ -55,7 +55,6 @@ export default function Home() {
 				}}
 				className="fixed inset-0 opacity-30"
 			/>
-
 			{/* Gradient */}
 			<Image
 				width={1200}
@@ -65,14 +64,88 @@ export default function Home() {
 				className="fixed inset-0 w-screen h-screen object-cover"
 				src={assets.gradient}
 			/>
-
 			{/* Reveal */}
 			<div
-				className={
-					(cn("bg-black fixed inset-0 transition-opacity duration-1000"),
-					!showBackground ? "opacity-100" : "opacity-0")
-				}
+				className={cn(
+					"bg-black fixed inset-0 transition-opacity duration-1000",
+					!showBackground ? "opacity-100" : "opacity-0"
+				)}
 			/>
+			{/* Content */}
+			<div className="max-w-7xl mt-20 mx-auto">
+				<div className="flex flex-col items-center relative z-10">
+					{/* Heading */}
+					<h1 className={`text-7xl max-w-3xl text-center leading-snug mb-12 ${poppins.className}`}>
+						<Image
+							alt="Figma logo"
+							className="inline-block mr-8 -mt-2"
+							src={assets.figma}
+							width="50"
+							height="50"
+						/>
+						to <FrameworkRotation currentFramework={currentFramework} /> will{" "}
+						<span
+							className={cn("transition-colors duration-200", {
+								"text-purple-300": currentFramework === "qwik",
+								"text-sky-300": currentFramework === "safari",
+								"text-yellow-300": currentFramework === "chrome",
+								"text-teal-300": currentFramework === "tailwind",
+								"text-blue-300": currentFramework === "react",
+								"text-green-300": currentFramework === "vue",
+								"text-orange-400": currentFramework === "svelte",
+								"text-red-300": currentFramework === "mobile",
+								"text-neutral-300": currentFramework === "desktop",
+							})}
+						>
+							never
+						</span>{" "}
+						be the same again
+					</h1>
+					{/* Sub heading */}
+					<p className="mb-8">
+						<span className="text-gray-300">Join us for an AI launch event by </span>
+						<Image
+							alt="Builder.io logo"
+							className="inline-block ml-1 -mt-1"
+							width={100}
+							height={20}
+							src={assets.builder}
+						/>
+						{" + "}
+						<Image
+							alt="Figma logo"
+							className="inline-block mx-1"
+							width={55}
+							height={20}
+							src={assets.figmatwo}
+						/>
+					</p>
+					{/* Claim ticket button */}
+					<div className="mb-8">
+						<button
+							ref={buttonRef}
+							className={cn(
+								"text-black px-6 py-3 rounded-md text-sm font-semibold transition-colors duration-200",
+								{
+									"bg-purple-300": currentFramework === "qwik",
+									"bg-sky-300": currentFramework === "safari",
+									"bg-yellow-300": currentFramework === "chrome",
+									"bg-teal-300": currentFramework === "tailwind",
+									"bg-blue-300": currentFramework === "react",
+									"bg-green-300": currentFramework === "vue",
+									"bg-orange-400": currentFramework === "svelte",
+									"bg-red-300": currentFramework === "mobile",
+									"bg-neutral-300": currentFramework === "desktop",
+								}
+							)}
+						>
+							Claim Ticket
+						</button>
+					</div>
+					{/* Countdown timer */}
+					<CountdownTimer currentFramework={currentFramework} />
+				</div>
+			</div>
 		</main>
 	);
 }
